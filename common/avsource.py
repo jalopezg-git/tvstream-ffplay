a2f07695abe78ff203fbac1958acec6c90570ae4
Javier Lopez-Gomez <36541918+jalopezg-git@users.noreply.github.com>
2023-08-13 22:14:55 +0200

    Add class `CurlMpegtsSequenceMuxAVSource`
    Similar to `CurlMpegtsSequenceAVSource` but spawns different `curl`
    processes to separately fetch the video and audio data.
    An extra `ffmpeg` process is used to multiplex both streams before feeding
    it to the sink.

--- common/avsource.py
+++ common/avsource.py
@@ -1,7 +1,9 @@
-import subprocess
 import logging
+import os
 import shlex
+import subprocess
 from abc import abstractmethod
+from threading import Thread
 
 class AVSource:
     """ Abstract interface for A/V sources """
@@ -47,16 +49,48 @@ class CurlMpegtsSequenceAVSource(AVSource):
             self.addHeaders += ['-H', 'Cookie: %s' % '; '.join(map(
                                 lambda c: c.name + '=' + c.value, cookies))]
 
-    def run(self, sink):
+    def curl_loop(curlArgv, urlTemplate, startAt, urlsPerProc, fhStdout):
         while True:
-            argv = ['curl', '--silent', '--fail', '--fail-early']
-            argv += self.addHeaders
-            argv += [self.urlTemplate % i for i in range(self.startAt,
-                                                         self.startAt + self.urlsPerProc)]
+            argv = curlArgv + [urlTemplate % i for i in range(startAt, startAt + urlsPerProc)]
+            startAt += urlsPerProc
 
             logging.debug('Spawning process: ' + shlex.join(argv))
-            curl = subprocess.Popen(argv, stdout=sink.stdin)
-            returncode = curl.wait()
-            if returncode != 0:
-                return (returncode == 22) # curl(1), EXIT CODES section: 22: HTTP page not retrieved
-            self.startAt += self.urlsPerProc
+            curl = subprocess.Popen(argv, stdout=fhStdout)
+            if curl.wait() != 0:
+                return
+
+    def run(self, sink):
+        CurlMpegtsSequenceAVSource.curl_loop(['curl', '--silent', '--fail', '--fail-early'] + self.addHeaders,
+                                             self.urlTemplate, self.startAt, self.urlsPerProc, sink.stdin)
+        return sink.poll() == None
+
+class CurlMpegtsSequenceMuxAVSource(CurlMpegtsSequenceAVSource):
+    """ An A/V source that spawns different `curl` processes to separately fetch
+    the video and audio data.  An extra `ffmpeg` process is used to multiplex
+    both streams before feeding it to the sink.
+
+    In contrast to `CurlMpegtsSequenceAVSource`, the constructor takes an array of
+    template URLs; an extra `curl` process is spawned for each element in the array.
+    ```
+    """
+    def run(self, sink):
+        threads = []
+        pipes = []
+        argv_ffmpeg_input = []
+        for urlTemplate in self.urlTemplate:
+            pipes += [os.pipe()]
+            argv_ffmpeg_input += ['-i', ('pipe:%i' % pipes[-1][0])]
+            curl_fhStdout = os.fdopen(pipes[-1][1], 'wb', buffering=0)
+            threads += [Thread(target=CurlMpegtsSequenceAVSource.curl_loop,
+                               args=[['curl', '--silent', '--fail', '--fail-early'] + self.addHeaders,
+                                     urlTemplate, self.startAt, self.urlsPerProc, curl_fhStdout])]
+            threads[-1].start()
+
+        mux = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet',
+                                '-c:v', 'copy', '-c:a', 'copy', '-f', 'mpegts', '-'] + argv_ffmpeg_input,
+                               stdout=sink.stdin, pass_fds=[p[0] for p in pipes])
+
+        # Close unused read end of the pipes
+        for p in pipes:
+            os.close(p[0])
+        return mux.wait() != 0 and sink.poll() == None
